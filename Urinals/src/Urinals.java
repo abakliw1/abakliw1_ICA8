8b467bf5f22d5b19c2a7a99d954d3c391951f490
Akshat Bakliwal <abakliw1@asu.edu>
2022-10-27 19:41:52 -0700

    Final commit with all test cases

--- Urinals/src/Urinals.java
+++ Urinals/src/Urinals.java
@@ -2,40 +2,52 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 public class Urinals {
-    public static String optFile() throws IOException {
+    public static String optFile(String path) throws Exception {
 
 //        System.out.println("File Chosen");;
-        File f=new File("src/urinal.dat");
-        BufferedReader br= new BufferedReader(new FileReader(f));
-        String st;
-        ArrayList<Integer> count=new ArrayList<Integer>();
-        while((st=br.readLine())!=null){
-            count.add(numuri(st));
-        }
-        for(int i=0;i< count.size();i++){
-            System.out.println(count.get(i));
-        }
-        String s = "";
-        for(int i=0;i<count.size();i++){
-            s+=" "+count.get(i);
-        }
-        br.close();
-        File f2=new File("src/rule.txt");
-        int counter=1;
-        while(f2.exists()){
-            f2=new File("src/rule"+counter+".txt");
-            counter++;
-        }
+        try{
+            File f=new File(path);
+            if(!f.exists())
+                throw new Exception("File Not Found!!");
+            else if(f.length()==0)
+                throw new Exception("File Empty");
+            else{
+
+                BufferedReader br= new BufferedReader(new FileReader(f));
+                String st;
+                ArrayList<Integer> count=new ArrayList<Integer>();
+                while((st=br.readLine())!=null){
+                    count.add(numuri(st));
+                }
+                for(int i=0;i< count.size();i++){
+                    System.out.println(count.get(i));
+                }
+                String s = "";
+                for(int i=0;i<count.size();i++){
+                    s+=" "+count.get(i);
+                }
+                br.close();
+                File f2=new File("src/rule.txt");
+                int counter=1;
+                while(f2.exists()){
+                    f2=new File("src/rule"+counter+".txt");
+                    counter++;
+                }
 //        f2=File.createNewFile("rule"+counter+".txt");
 //        System.out.println(f2.getName());
-        FileWriter fw=new FileWriter(f2);
-        for(int i=0;i<count.size();i++){
-            fw.write(count.get(i)+"\n");
-        }
-        fw.close();
-        return s;
-//        return "File";
+                FileWriter fw=new FileWriter(f2);
+                for(int i=0;i<count.size();i++){
+                    fw.write(count.get(i)+"\n");
+                }
+                fw.close();
+                return s;
+            }
 
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
     }
     public static int numuri(String str){
         int count=0;
@@ -99,17 +111,17 @@ public class Urinals {
         String s= String.valueOf(n);
         return s;
     }
-    public static String optStr(String s) throws IOException {
+    public static String optStr(String s) throws Exception {
         if(s.equals("Input")){
             return optIn();
         }
         else if(s.equals("File")){
-            return optFile();
+            return optFile("src/urinal.dat");
         }
         return "Wrong Option Chosen";
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         System.out.println("Enter the input method(Input for keyboard and File for File input): ");
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
--- Urinals/test/UrinalTest.java
+++ Urinals/test/UrinalTest.java
@@ -6,27 +6,25 @@ import static  org.junit.Assert.*;
 
 public class UrinalTest {
     Urinals tester=new Urinals();
-    @Test
-    public void testoptStr() throws IOException {
-        assertEquals("===Akshat Bakliwal=== File must be chosen Test Case 1", "File",tester.optStr("File"));
-        assertEquals("===Akshat Bakliwal=== Input must be chosen Test Case 2","Input",tester.optStr("Input"));
-    }
+
     @Test
     public void testgoodString(){
-        assertEquals("====Akshat Bakliwal===== should return -1 Test Case 3",-1,tester.goodString("1100"));
-        assertEquals("====Akshat Bakliwal===== should return -1 Test Case 4",-1,tester.goodString("110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"));
+        assertEquals("====Akshat Bakliwal===== should return -1 Test Case 1",-1,tester.goodString("1100"));
+        assertEquals("====Akshat Bakliwal===== should return -1 Test Case 2",-1,tester.goodString("110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"));
     }
     @Test
     public void testnumuri(){
-        assertEquals("===Akshat Bakliwal===should return 1 Test Case 5",1,tester.numuri("10001"));
-        assertEquals("===Akshat Bakliwal===should return 0 Test Case 6",0,tester.numuri("1001"));
-        assertEquals("===Akshat Bakliwal===should return 3 Test Case 7",3,tester.numuri("00000"));
-        assertEquals("===Akshat Bakliwal===should return 2 Test Case 8",2,tester.numuri("0000"));
-        assertEquals("===Akshat Bakliwal===should return 1 Test Case 9",1,tester.numuri("01000"));
+        assertEquals("===Akshat Bakliwal===should return 1 Test Case 3",1,tester.numuri("10001"));
+        assertEquals("===Akshat Bakliwal===should return 0 Test Case 4",0,tester.numuri("1001"));
+        assertEquals("===Akshat Bakliwal===should return 3 Test Case 5",3,tester.numuri("00000"));
+        assertEquals("===Akshat Bakliwal===should return 2 Test Case 6",2,tester.numuri("0000"));
+        assertEquals("===Akshat Bakliwal===should return 1 Test Case 7",1,tester.numuri("01000"));
     }
     @Test
-    public void testoptFile() throws IOException{
-        assertEquals("===Akshat Bakliwal===should return 1 Test Case 10"," 1 0 3 2 1",tester.optFile());
+    public void testoptFile() throws Exception{
+        assertEquals("===Akshat Bakliwal===should return the array Test Case 8"," 1 0 3 2 1",tester.optFile("src/urinal.dat"));
+        assertEquals("===Akshat Bakliwal===should return throw exception Test Case 9",null,tester.optFile("src/uri.dat"));
+        assertEquals("===Akshat Bakliwal===should return throw exception Test Case 10",null,tester.optFile("src/nuri.dat"));
     }
 
 }
